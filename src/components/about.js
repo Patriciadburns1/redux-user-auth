7627afe256838f3c7f12ce21ab06ec26d6288a64
dburns1 <patriciadburns@gmail.com>
2018-07-11 15:36:30 -0700

    added Nav bar component

---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+export default props => {
+    return (
+        <div> 
+            <h1 className="text-center"> ABOUT US  </h1> 
+            <p>BB BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB</p>
+            <p>AHHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAH </p> 
+        </div> 
+    )
+}
